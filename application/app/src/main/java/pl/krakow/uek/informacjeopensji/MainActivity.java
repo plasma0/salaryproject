976c3853380e8a3d25d6fda0cbfa334f0c5d0d00
plasma0 <konrad_kania@wp.pl>
2017-05-27 21:20:18 +0200

    Dodanie więcej pól członków rodziny

--- application/app/src/main/java/pl/krakow/uek/informacjeopensji/MainActivity.java
+++ application/app/src/main/java/pl/krakow/uek/informacjeopensji/MainActivity.java
@@ -20,23 +20,47 @@ public class MainActivity extends AppCompatActivity {
         ((EditText)findViewById(R.id.editText8)).setVisibility(View.INVISIBLE);
         ((EditText)findViewById(R.id.editText7)).setVisibility(View.INVISIBLE);
         ((EditText)findViewById(R.id.editText6)).setVisibility(View.INVISIBLE);
+        ((EditText)findViewById(R.id.editText)).setVisibility(View.INVISIBLE);
+        ((EditText)findViewById(R.id.editText2)).setVisibility(View.INVISIBLE);
+        ((EditText)findViewById(R.id.editText3)).setVisibility(View.INVISIBLE);
         state = 0;
     }
 
     public Integer state;
 
     public void adder(View view) {
-        state++;
-        switch (state)
-        {
-            case 1:((EditText)findViewById(R.id.editText9)).setVisibility(View.VISIBLE);break;
-            case 2:((EditText)findViewById(R.id.editText8)).setVisibility(View.VISIBLE);break;
-            case 3:((EditText)findViewById(R.id.editText7)).setVisibility(View.VISIBLE);break;
-            case 4:((EditText)findViewById(R.id.editText6)).setVisibility(View.VISIBLE);break;
-            default:break;
+
+        if(state<6) {
+            state++;
+            switch (state) {
+                case 1:
+                    ((EditText) findViewById(R.id.editText9)).setVisibility(View.VISIBLE);
+                    break;
+                case 2:
+                    ((EditText) findViewById(R.id.editText8)).setVisibility(View.VISIBLE);
+                    break;
+                case 3:
+                    ((EditText) findViewById(R.id.editText7)).setVisibility(View.VISIBLE);
+                    break;
+                case 4:
+                    ((EditText) findViewById(R.id.editText6)).setVisibility(View.VISIBLE);
+                    break;
+                case 5:
+                    ((EditText) findViewById(R.id.editText)).setVisibility(View.VISIBLE);
+                    break;
+                case 6:
+                    ((EditText) findViewById(R.id.editText2)).setVisibility(View.VISIBLE);
+                    break;
+                case 7:
+                    ((EditText) findViewById(R.id.editText3)).setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+
+            ((TextView) findViewById(R.id.numx)).setText(state.toString());
         }
 
-        ((TextView)findViewById(R.id.numx)).setText(state.toString());
     }
 
     public void deleter(View view) {
@@ -61,6 +85,17 @@ public class MainActivity extends AppCompatActivity {
                     m3.setVisibility(View.INVISIBLE);
                     m3.setText("0");
                     break;
+                case 4:EditText m4 = (EditText)findViewById(R.id.editText);
+                    m4.setVisibility(View.INVISIBLE);
+                    m4.setText("0");
+                    break;
+                case 5:EditText m5 = (EditText)findViewById(R.id.editText2);
+                    m5.setVisibility(View.INVISIBLE);
+                    m5.setText("0");
+                    break;
+                case 6:EditText m6 = (EditText)findViewById(R.id.editText3);
+                    m6.setVisibility(View.INVISIBLE);
+                    m6.setText("0");
                 default:break;
             }
 
@@ -76,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
         summa += Float.parseFloat(((EditText)findViewById(R.id.editText8)).getText().toString());
         summa += Float.parseFloat(((EditText)findViewById(R.id.editText7)).getText().toString());
         summa += Float.parseFloat(((EditText)findViewById(R.id.editText6)).getText().toString());
+        summa += Float.parseFloat(((EditText)findViewById(R.id.editText2)).getText().toString());
+        summa += Float.parseFloat(((EditText)findViewById(R.id.editText3)).getText().toString());
+        summa += Float.parseFloat(((EditText)findViewById(R.id.editText)).getText().toString());
         SPe.putFloat("isalary",summa);
         SPe.putInt("members",state);
         SPe.commit();
